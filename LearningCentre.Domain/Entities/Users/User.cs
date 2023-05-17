2833311966ab0e660bdc119ee1419125502f875c
Behzodkhoja <nigmatxojayevbehzodxoja@gmail.com>
2023-05-17 19:05:02 +0500

    aa

--- LearningCentre.Domain/Entities/Users/User.cs
+++ LearningCentre.Domain/Entities/Users/User.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LearningCentre.Domain.Commons;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,13 @@ using System.Threading.Tasks;
 
 namespace LearningCentre.Domain.Entities.Users
 {
-    public class User
+    public class User:Auditable
     {
-
+        public string Group { get; set; }
+        public string Pay { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Direction { get; set; }
+        public string PhoneNumber { get; set; }
     }
 }
