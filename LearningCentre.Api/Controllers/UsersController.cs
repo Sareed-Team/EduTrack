7cb2c288ef7c0f42fd770d8ca153c26145a1ece4
Saeed <dotnettashkent@gmail.com>
2023-05-17 20:47:37 +0500

    Added other classes

--- LearningCentre.Api/Controllers/UsersController.cs
+++ LearningCentre.Api/Controllers/UsersController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LearningCentre.Service.DTOs.Users;
+using LearningCentre.Service.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LearningCentre.Api.Controllers
 {
@@ -6,5 +8,19 @@ namespace LearningCentre.Api.Controllers
     [ApiController]
     public class UsersController : BaseController
     {
+        private readonly IUserService userService;
+
+        public UsersController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+        [HttpPost]
+        public async Task<IActionResult> PostStudents(UserCreationDto dto)
+            => Ok(new
+            {
+                Code = 200,
+                Error = "Succes",
+                Data = await this.userService.CreateAsync(dto)
+            });
     }
 }
---
+++ LearningCentre.Api/Extensions/ServiceExtension.cs
@@ -0,0 +1,16 @@
+﻿using LearningCentre.DAL.IRepository;
+using LearningCentre.DAL.Repository;
+using LearningCentre.Service.Interfaces;
+using LearningCentre.Service.Services;
+
+namespace LearningCentre.Api.Extensions
+{
+    public static class ServiceExtension
+    {
+        public static void AddCustomServices(this IServiceCollection services)
+        {
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped(typeof(IRepository<>),typeof(Repository<>));
+        }
+    }
+}
