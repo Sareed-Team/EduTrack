006dc25b19a0e85d1c4906d0fcd4be1d062bac8e
Behzodkhoja <nigmatxojayevbehzodxoja@gmail.com>
2023-05-17 19:04:01 +0500

    Updated

---
+++ LearningCentre.Domain/Entities/Users/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningCentre.Domain.Entities.Users
+{
+    public class User
+    {
+
+    }
+}
